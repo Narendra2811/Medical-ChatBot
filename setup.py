e92d49b15b829d2cd7614504358d5cce621620ff
Narendra Patel <np28112004@gmail.com>
2026-04-14 23:58:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Medical-ChatBot',
+    version='0.1.0',
+    author='Narendra Patel',
+    packages=find_packages(),
+    install_requires=[]
+    
+)
